23e083c8587006a47085e435f1c96dc2ed643506
SBoyarkin <sergey.boyarkin@mail.ru>
2024-04-02 23:02:18 +0500

    Задача №1

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 
-def createCookBook(file):
+def create_cook_book(file):
     cook_book = {}
     cook = None
     for line in file:
         if '|' not in line and not line.isdigit() and line:
             cook_book[line] = []
-            cook=line
+            cook = line
         if '|' in line:
             line = line.split('|')
-            cook_book[cook].append({'ingredient_name': line[0], 'quantity':line[1], 'measure':line[2]})
+            cook_book[cook].append({'ingredient_name': line[0], 'quantity': line[1], 'measure': line[2]})
     return cook_book
 
 
 with open('recipes.txt') as f:
     data = f.read().splitlines()
-    cook_book = createCookBook(data)
+    book = create_cook_book(data)
     f.close()
 
-print(cook_book)
+print(book)
